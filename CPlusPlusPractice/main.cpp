d501ea81144e58c777b6539b4b4143887905cc3e
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-12 15:44:33 -0400

    Practiced with C++ Variables

--- CPlusPlusPractice/main.cpp
+++ CPlusPlusPractice/main.cpp
@@ -7,8 +7,14 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int x = 5;
+    int y = 6;
+    int sum = x+y;
+    
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    std::cout << sum << "\n";
+    
     return 0;
 }
