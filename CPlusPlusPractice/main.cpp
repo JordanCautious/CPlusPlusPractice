e0cc94916eec262daa7f601ee28a049432bc1d26
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-12 00:32:56 -0400

    Initial Commit

---
+++ CPlusPlusPractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPlusPlusPractice
+//
+//  Created by Jordan Haynes on 7/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
